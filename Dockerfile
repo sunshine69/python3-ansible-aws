8b217d1d1b157c9a6c700412c0cf1c5a92d14d63
Gitea <gitea@fake.local>
2018-11-03 01:32:49 +0000

    Add some common python modules
    also probably drop support for clearlinux image as it is not quite needed.

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,14 @@ ARG ANSIBLE_VERSION=${ANSIBLE_VERSION:-2.6.4}
 
 RUN apt-get -y update && apt-get -y upgrade 
 RUN apt-get install -y python3 python3-pip git rsync p7zip zip vim \
+    python3-dev default-libmysqlclient-dev \
     && ln -sf /usr/bin/pip3 /usr/bin/pip \
     && ln -sf /usr/bin/python3 /usr/bin/python
 
 RUN pip3 install --upgrade pip \
     && pip install botocore boto3 boto awscli \
+    pyotp qrcode[pil] requests pymssql beautifulsoup4 psycopg2 \
+    netaddr pexpect pycrypto cryptography pytz mysqlclient \
     && pip install ansible==$ANSIBLE_VERSION
 
 RUN apt-get clean && rm -rf ~/.cache
--- build.sh
+++ build.sh
@@ -4,29 +4,31 @@ for base_image in ubuntu:latest alpine:latest amd64/clearlinux:latest; do
     echo docker pull $base_image
 done
 
-export ANSIBLE_VERSION=2.7.0
+export ANSIBLE_VERSION=2.7.1
 
-docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION -f Dockerfile.clearlinux .
-docker tag xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION xvtsolutions/clearlinux-python3-aws-ansible:latest
-docker push xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION
-docker push xvtsolutions/clearlinux-python3-aws-ansible:latest
+#docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION -f Dockerfile.clearlinux .
+#docker tag xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION xvtsolutions/clearlinux-python3-aws-ansible:latest
+#docker push xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION
+#docker push xvtsolutions/clearlinux-python3-aws-ansible:latest
 
 docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/alpine-python3-aws-ansible:$ANSIBLE_VERSION -f Dockerfile.alpine .
 docker tag xvtsolutions/alpine-python3-aws-ansible:$ANSIBLE_VERSION xvtsolutions/alpine-python3-aws-ansible:latest
 docker push xvtsolutions/alpine-python3-aws-ansible:$ANSIBLE_VERSION
 docker push xvtsolutions/alpine-python3-aws-ansible:latest
 
-docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION -f Dockerfile-python2.clearlinux .
-docker tag xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION xvtsolutions/clearlinux-python2-aws-ansible:latest
-docker push xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION
-docker push xvtsolutions/clearlinux-python2-aws-ansible:latest
+#docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION -f Dockerfile-python2.clearlinux .
+#docker tag xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION xvtsolutions/clearlinux-python2-aws-ansible:latest
+#docker push xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION
+#docker push xvtsolutions/clearlinux-python2-aws-ansible:latest
 
-docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION -f Dockerfile-python2.alpine .
-docker tag xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION xvtsolutions/alpine-python2-aws-ansible:latest
-docker push xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION
-docker push xvtsolutions/alpine-python2-aws-ansible:latest
+#docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION -f Dockerfile-python2.alpine .
+#docker tag xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION xvtsolutions/alpine-python2-aws-ansible:latest
+#docker push xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION
+#docker push xvtsolutions/alpine-python2-aws-ansible:latest
 
 docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/python3-aws-ansible:$ANSIBLE_VERSION -f Dockerfile .
 docker tag xvtsolutions/python3-aws-ansible:$ANSIBLE_VERSION xvtsolutions/python3-aws-ansible:latest
+echo "Push images?"
+read _junk
 docker push xvtsolutions/python3-aws-ansible:$ANSIBLE_VERSION
 docker push xvtsolutions/python3-aws-ansible:latest
