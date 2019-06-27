8c8bd2a4ac081b1d960484cad78a94af0dca75e7
Gitea <gitea@fake.local>
2019-06-27 10:10:21 +1000

    Update build script, bump ansible version
    some minor improvement in build scripts

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ for base_image in ubuntu:latest alpine:latest amd64/clearlinux:latest; do
     echo docker pull $base_image
 done
 
-export ANSIBLE_VERSION="2.7.10"
+#export ANSIBLE_VERSION="2.7.11"
+export ANSIBLE_VERSION="2.8.1"
 
 build_clearlinux() {
 #docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION -f Dockerfile.clearlinux .
 #docker tag xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION xvtsolutions/clearlinux-python3-aws-ansible:latest
 #docker push xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION
 #docker push xvtsolutions/clearlinux-python3-aws-ansible:latest
+echo "Not supported"
 return
 }
 
@@ -26,6 +28,7 @@ build_clearlinux_python2() {
 #docker tag xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION xvtsolutions/clearlinux-python2-aws-ansible:latest
 #docker push xvtsolutions/clearlinux-python2-aws-ansible:$ANSIBLE_VERSION
 #docker push xvtsolutions/clearlinux-python2-aws-ansible:latest
+echo "Not supported"
 return
 }
 
@@ -34,6 +37,7 @@ build_alpine_python2() {
 #docker tag xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION xvtsolutions/alpine-python2-aws-ansible:latest
 #docker push xvtsolutions/alpine-python2-aws-ansible:$ANSIBLE_VERSION
 #docker push xvtsolutions/alpine-python2-aws-ansible:latest
+echo "Not supported"
 return
 }
 
@@ -52,8 +56,18 @@ docker push xvtsolutions/python2-aws-ansible:latest
 }
 
 #### Execution
-echo "Edit this and uncomment the image you want to build"
-build_ubuntu
-build_ubuntu_python2
-build_alpine
-build_alpine_python2
+echo "Build ubuntu image? y/n "
+read ans
+[ "$ans" == "y" ] && build_ubuntu && ans='n'
+
+echo "Build ubuntu python2 image? y/n "
+read ans
+[ "$ans" == "y" ] && build_ubuntu_python2 && ans='n'
+
+echo "Build alpine image? y/n "
+read ans
+[ "$ans" == "y" ] && build_alpine && ans='n'
+
+echo "Build alpine python2 image? y/n "
+read ans
+[ "$ans" == "y" ] && build_alpine_python2 && ans='n'
