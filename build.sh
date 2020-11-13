1631d814ad18bff9d78fb1480561c2d7d3f2c73e
steve kieu <msh.computing@gmail.com>
2020-11-13 02:01:18 +0000

    Update to ansible 2.10.3

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ for base_image in ubuntu:19.04 alpine:latest; do
     docker pull $base_image
 done
 
-export ANSIBLE_VERSION="2.9.5"
+export ANSIBLE_VERSION="2.10.3"
 
 build_clearlinux() {
 docker build --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION -t xvtsolutions/clearlinux-python3-aws-ansible:$ANSIBLE_VERSION -f Dockerfile.clearlinux .
